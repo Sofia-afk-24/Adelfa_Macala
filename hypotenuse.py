171acfb943205a0d4dc8b283eaba17b788867cb3
Sofia-afk-24 <phmacala@carc.pshs.edu.ph>
2026-09-24 16:39:42 +0800

    Add files via upload
    I fixed the spacing and formatting for users to understand the flow of the code clearly from the perspective of the developer.

--- hypotenuse.py
+++ hypotenuse.py
@@ -1,7 +1,13 @@
 # Hypotenuse of a Right Triangle Calculator
+# Author: Princess Sofia H. Macala
+# Date: September 15, 2026
 
 ## Description
 ### This program calculates the hypotenuse of a right triangle using input values and a math library.
+
+# ======================================================================================================================
+# INPUT: Get triangle's information with clear prompts from the user.
+# ======================================================================================================================
 
 # Use of the math library in order for "math.sqrt()" and "pow()" to function.
 import math
@@ -10,8 +16,13 @@ import math
 a =float(input("Enter the length of side a: "))
 b =float(input("Enter the length of side b: "))
 
+# ======================================================================================================================
+# PROCESS AND OUTPUT: Calculate for the hypotenuse.
+# ======================================================================================================================
+
 ## Calculation of the hypotenuse using the Pythagorean theorem.
 hypotenuse =math.sqrt(pow(a,2)+pow(b,2))
 
 ## Output of the result.
 print(f"The hypotenuse is: {hypotenuse:.2f}")
+
