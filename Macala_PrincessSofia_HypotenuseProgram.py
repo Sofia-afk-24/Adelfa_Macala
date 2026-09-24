4382f119295ca9069d984789bc16873948b9ad0c
Sofia-afk-24 <phmacala@carc.pshs.edu.ph>
2026-09-24 16:47:57 +0800

    Add files via upload
    This was my original file.

---
+++ Macala_PrincessSofia_HypotenuseProgram.py
@@ -0,0 +1,13 @@
+# ============================================================
+# Name: Princess Sofia H. Macala
+# Section: 8 Adelfa
+# ============================================================
+
+import math
+
+a =float(input("Enter the length of side a: "))
+b =float(input("Enter the length of side b: "))
+
+hypotenuse =math.sqrt(pow(a,2)+pow(b,2))
+
+print(f"The hypotenuse is: {hypotenuse:.2f}")
